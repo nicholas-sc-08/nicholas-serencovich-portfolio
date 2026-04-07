753af3ce905e35f322716d0ed00f7d7c97ac90f2
nicholas-sc-08 <nicholassc.2008@gmail.com>
2026-04-07 17:25:29 -0300

    feat: versão em inglês

--- src/dictionaries/en.ts
+++ src/dictionaries/en.ts
@@ -12,8 +12,250 @@ export const en = {
         paragraphFour: "Today, I move between the robustness of the backend and the fluidity of the modern frontend with Next.js and TypeScript. For me, Computer Science is the art of solving problems through elegant abstractions. Coming from a public school background and carving out my space through code, I’ve learned that luck is not an engineering factor."
     },
     sectionThree: {
-      title: "Projects",
-      projects: []
+        title: "Projects",
+        projects: [
+            {
+                title: "Fly - Circular",
+                image: "https://github.com/nicholas-sc-08/Projeto-SA-3a-Modulo-SENAI/raw/main/Imagens%20do%20README/Banner%20Fly.png",
+                description: "Fly is an innovative application designed to empower thrift stores and simplify the journey for sustainable fashion enthusiasts. The platform connects users with unique local pieces, enabling practical and efficient eco-friendly shopping. Inspired by the real-world challenges faced by small entrepreneurs in the fashion industry, Fly serves as a technological bridge that promotes conscious consumption while valuing clothing with a history.",
+                technologies: [
+                    { image: "/icons/next.svg", alt: "next" },
+                    { image: "/icons/nodejs.svg" },
+                    { image: "/icons/express.svg", alt: "ex" },
+                    { image: "/icons/mongodb.svg", alt: "mongodb" },
+                    { image: "/icons/docker.svg", alt: "docker" },
+                    { image: "/icons/axios.svg", alt: "axios" },
+                    { image: "/icons/javascript.svg", alt: "javascript" },
+                    { image: "/icons/typescript.svg", alt: "typescript" },
+                    { image: "/icons/jwt.svg" },
+                    { image: "/icons/stripe.svg" }
+                ]
+            },
+            {
+                title: "Island",
+                description: "Discover the true 'Magic Island' through the eyes of Tainho, an enthusiastic local adventurer who plans curated weekly journeys for tourists. Island explores authentic trails, hidden beaches, and local restaurants to boost the regional economy while preventing overcrowding. By intelligently diluting tourist flow, Tainho ensures a peaceful experience and contributes to the island's environmental preservation. Follow Tainho's lead for an unforgettable, sustainable adventure!",
+                image: "https://github.com/nicholas-sc-08/island-web/raw/main/public/IslandBanner.png",
+                technologies: [
+                    { image: "/icons/next.svg", alt: "next" },
+                    { image: "/icons/nodejs.svg", alt: "nodejs" },
+                    { image: "/icons/express.svg", alt: "ex" },
+                    { image: "/icons/postgresql.svg", alt: "postgresql" },
+                    { image: "/icons/prisma.svg", alt: "prisma" },
+                    { image: "/icons/docker.svg", alt: "docker" },
+                    { image: "/icons/shadcn.svg", alt: "shadcn" },
+                    { image: "/icons/tailwindcss.svg", alt: "tailwind" },
+                    { image: "/icons/jwt.svg", alt: "jwt" }
+                ]
+            },
+            {
+                title: "VortexPay",
+                description: "VortexPay is a high-performance banking ecosystem API engineered to process the core operations of a modern fintech. Inspired by the robustness of leading financial institutions, this project acts as the central transactional engine, orchestrating complex flows ranging from secure access authentication to atomic asset settlement.",
+                image: "https://github.com/nicholas-sc-08/vortexpay-api/raw/main/vortexpay_banner.png",
+                technologies: [
+                    { image: "/icons/next.svg", alt: "next" },
+                    { image: "icons/redux.svg", alt: "redux" },
+                    { image: "/icons/shadcn.svg", alt: "shadcn" },
+                    { image: "/icons/java.svg", alt: "java" },
+                    { image: "/icons/spring.svg", alt: "spring" },
+                    { image: "/icons/hibernate.svg", alt: "hibernate" },
+                    { image: "/icons/maven.svg", alt: "maven" },
+                    { image: "/icons/junit.svg", alt: "junit" },
+                    { image: "/icons/docker.svg", alt: "docker" },
+                    { image: "/icons/kubernetes.svg", alt: "kubernetes" },
+                    { image: "/icons/postgresql.svg", alt: "postgresql" }
+                ]
+            },
+            {
+                title: "Open Weather",
+                description: "A responsive web application designed for real-time global weather consulting. The project features a robust RESTful API built with Java and Spring Boot, which intelligently processes data from the OpenWeather API and manages data persistence through a MySQL database, focusing on high availability and sleek UI.",
+                image: "/openweather.jpg",
+                technologies: [
+                    { image: "/icons/next.svg", alt: "next" },
+                    { image: "/icons/shadcn.svg", alt: "shadcn" },
+                    { image: "/icons/framermotion.svg", alt: "framermotion" },
+                    { image: "/icons/tailwindcss.svg", alt: "tailwind" },
+                    { image: "/icons/typescript.svg", alt: "typescript" },
+                    { image: "/icons/java.svg", alt: "java" },
+                    { image: "/icons/spring.svg", alt: "spring" },
+                    { image: "/icons/maven.svg", alt: "maven" },
+                    { image: "/icons/swagger.svg", alt: "swagger" },
+                    { image: "/icons/mysql.svg", alt: "mysql" }
+                ]
+            },
+            {
+                title: "FreshLog",
+                description: "FreshLog is a specialized inventory management system designed to solve the critical challenges of handling perishable goods. Unlike generic stock systems, the API focuses on batch-level traceability and shelf-life optimization through smart algorithms. It is the ideal solution for operations requiring absolute precision in expiration control and strategic waste reduction.",
+                image: "/freshlog.jpg",
+                technologies: [
+                    { image: "/icons/java.svg", alt: "java" },
+                    { image: "/icons/spring.svg", alt: "spring" },
+                    { image: "/icons/hibernate.svg", alt: "hibernate" },
+                    { image: "/icons/maven.svg", alt: "maven" },
+                    { image: "/icons/junit.svg", alt: "junit" },
+                    { image: "/icons/swagger.svg", alt: "swagger" },
+                    { image: "/icons/docker.svg", alt: "docker" },
+                    { image: "/icons/mysql.svg", alt: "mysql" }
+                ]
+            },
+            {
+                title: "Enterprise Management",
+                description: "This application is a robust CRUD prototype developed to support the organization of Santa Catarina's entrepreneurial ecosystem. The solution manages crucial data on companies and stakeholders, categorized by industry segments and municipalities. Built with a focus on scalability and maintainability, it utilizes a layered architecture following modern software development best practices.",
+                image: "/sctec.jpg",
+                technologies: [
+                    { image: "/icons/java.svg", alt: "java" },
+                    { image: "/icons/spring.svg", alt: "spring" },
+                    { image: "/icons/maven.svg", alt: "maven" },
+                    { image: "/icons/junit.svg", alt: "junit5" },
+                    { image: "/icons/mysql.svg", alt: "mysql" },
+                    { image: "/icons/docker.svg", alt: "docker" },
+                    { image: "/icons/swagger.svg", alt: "swagger" }
+                ]
+            },
+            {
+                title: "Alextur Floripa",
+                description: "Alextur Floripa redefines executive transportation as a strategic corporate mobility partner. Specializing in premium transfers, the service combines a modern fleet with a highly selected professional team. The result is a travel experience defined by absolute punctuality, safety, and the comfort necessary for clients to focus solely on their business success.",
+                image: "/alextur.jpg",
+                technologies: [
+                    { image: "/icons/vitejs.svg", alt: "vite" },
+                    { image: "/icons/react.svg", alt: "react" },
+                    { image: "/icons/reactrouter.svg", alt: "reactrouter" },
+                    { image: "/icons/javascript.svg", alt: "javascript" },
+                    { image: "/icons/css3.svg", alt: "css" }
+                ]
+            },
+            {
+                title: "OncoMed",
+                description: "OncoMed is a comprehensive healthcare platform designed to streamline medical access. It integrates practical online appointment scheduling with a reliable information portal. The blog provides essential support for patients seeking to understand symptoms and specific care, transforming the patient journey into a more informed, secure, and connected experience.",
+                image: "https://media.licdn.com/dms/image/v2/D4D2DAQHtpW8KDGuHMQ/profile-treasury-image-shrink_800_800/B4DZvcA_avJsAc-/0/1768922806364?e=1776193200&v=beta&t=5dVhglEJ-zkPwbIUo7XK2lTI5YdTf0s2qTYJ-sIxBjM",
+                technologies: [
+                    { image: "/icons/vitejs.svg", alt: "vite" },
+                    { image: "/icons/react.svg", alt: "react" },
+                    { image: "/icons/reactrouter.svg", alt: "reactrouter" },
+                    { image: "/icons/javascript.svg", alt: "javascript" },
+                    { image: "/icons/css3.svg", alt: "css3" }
+                ]
+            },
+            {
+                title: "BabyLand",
+                description: "BabyLand is an exclusive caregiving mediation platform designed for those who prioritize safety and excellence. Users can select highly qualified professionals based on real reviews and detailed profiles. Our technology facilitates direct contact to align expectations even before scheduling, ensuring peace of mind for families and safety for loved ones.",
+                image: "https://media.licdn.com/dms/image/v2/D4D2DAQEn-sRIWNgzAA/profile-treasury-image-shrink_1920_1920/B4DZvcDQCOK0Ac-/0/1768923398706?e=1776193200&v=beta&t=4XRHu1RA5DL69Ep5dqCDVxtdw4Mgks00HHKS7pX_9Eg",
+                technologies: [
+                    { image: "/icons/html5.svg", alt: "html5" },
+                    { image: "/icons/javascript.svg", alt: "javascript" },
+                    { image: "/icons/css3.svg", alt: "css3" }
+                ]
+            },
+            {
+                title: "Story Cubes",
+                description: "Unleash your inner storyteller with Story Cubes, an imagination game that invites you to explore fantastic worlds through 9 exclusive dice. With 54 varied icons, every roll generates a unique combination of images that serve as sparks for epic, funny, or mysterious narratives. Where logic ends, your creativity begins.",
+                image: "/storycubes.jpg",
+                technologies: [
+                    { image: "/icons/vitejs.svg", alt: "vite" },
+                    { image: "/icons/react.svg", alt: "react" },
+                    { image: "/icons/nodejs.svg", alt: "nodejs" },
+                    { image: "/icons/javascript.svg", alt: "javascript" },
+                    { image: "/icons/html5.svg", alt: "html5" },
+                    { image: "/icons/css3.svg", alt: "css3" }
+                ]
+            },
+            {
+                title: "Food King",
+                description: "At FoodKing, we believe that eating well is a royal right. Our Landing Page was designed to offer a complete gastronomic journey where high-quality ingredients meet delivery speed. Explore a curated menu and experience the standard of excellence that makes us leaders in customer satisfaction. Whatever your craving, the king of food delivers.",
+                image: "/foodking.jpg",
+                technologies: [
+                    { image: "/icons/vitejs.svg", alt: "vite" },
+                    { image: "/icons/react.svg", alt: "react" },
+                    { image: "/icons/reactrouter.svg", alt: "reactrouter" },
+                    { image: "/icons/nodejs.svg", alt: "node" },
+                    { image: "/icons/javascript.svg", alt: "javascript" },
+                    { image: "/icons/css3.svg", alt: "css3" }
+                ]
+            },
+            {
+                title: "Tracking Vault",
+                description: "Developed with React, this landing page is designed to connect artists with their audience through a modern, intuitive interface. Primarily focused on promoting musical performances, it displays dates and venues with high visual impact. It features a refined responsive design to ensure an engaging user experience across all devices, facilitating fan conversion.",
+                image: "/trackingvault.jpg",
+                technologies: [
+                    { image: "/icons/vitejs.svg", alt: "vite" },
+                    { image: "/icons/react.svg", alt: "react" },
+                    { image: "/icons/reactrouter.svg", alt: "reactrouter" },
+                    { image: "/icons/nodejs.svg", alt: "node" },
+                    { image: "/icons/javascript.svg", alt: "javascript" },
+                    { image: "/icons/css3.svg", alt: "css3" }
+                ]
+            },
+            {
+                title: "Pata Feliz",
+                description: "Pata Feliz is a vibrant and intuitive landing page built with React, designed to provide a welcoming digital experience. The project focuses on converting visitors into animal cause supporters through a modern, interactive, and fully responsive interface that highlights the importance of pet adoption and care.",
+                image: "/patafeliz.jpg",
+                technologies: [
+                    { image: "/icons/vitejs.svg", alt: "vite" },
+                    { image: "/icons/react.svg", alt: "react" },
+                    { image: "/icons/reactrouter.svg", alt: "reactrouter" },
+                    { image: "/icons/nodejs.svg", alt: "node" },
+                    { image: "/icons/javascript.svg", alt: "javascript" },
+                    { image: "/icons/css3.svg", alt: "css3" }
+                ]
+            },
+            {
+                title: "Positivus",
+                description: "Positivus is a digital solutions agency focused on transforming brands through assertive strategies and high-impact design. This landing page reflects the agency's essence: clarity, dynamism, and a results-oriented mindset. It combines cutting-edge technology with an intuitive interface to ensure a seamless customer journey.",
+                image: "/positivus.jpg",
+                technologies: [
+                    { image: "/icons/vitejs.svg", alt: "vite" },
+                    { image: "/icons/react.svg", alt: "react" },
+                    { image: "/icons/reactrouter.svg", alt: "reactrouter" },
+                    { image: "/icons/nodejs.svg", alt: "node" },
+                    { image: "/icons/javascript.svg", alt: "javascript" },
+                    { image: "/icons/css3.svg", alt: "css3" }
+                ]
+            },
+            {
+                title: "Pharmacy System",
+                description: "What began as an academic project in React evolved into a complete management and sales interface for pharmacies. The platform prioritizes easy navigation and quick access to medications and personal care items. It combines a clean aesthetic with an efficient component architecture, ensuring agility in customer service and inventory control.",
+                image: "/farmacia.jpg",
+                technologies: [
+                    { image: "/icons/vitejs.svg", alt: "vite" },
+                    { image: "/icons/react.svg", alt: "react" },
+                    { image: "/icons/reactrouter.svg", alt: "reactrouter" },
+                    { image: "/icons/nodejs.svg", alt: "node" },
+                    { image: "/icons/javascript.svg", alt: "javascript" },
+                    { image: "/icons/css3.svg", alt: "css3" }
+                ]
+            },
+            {
+                title: "Simple Calculator",
+                description: "A high-performance calculator application designed to provide instantaneous responses and a fluid interface. This project utilizes Vite for an ultra-fast development environment and React for efficient, reactive state management, featuring a clean and modern UI.",
+                image: "/calculadorasimples.jpg",
+                technologies: [
+                    { image: "/icons/vitejs.svg", alt: "vite" },
+                    { image: "/icons/react.svg", alt: "react" },
+                    { image: "/icons/nodejs.svg", alt: "node" },
+                    { image: "/icons/javascript.svg", alt: "javascript" },
+                    { image: "/icons/css3.svg", alt: "css3" }
+                ]
+            },
+            {
+                title: "Quiz Chess",
+                description: "An engaging quiz application focused on chess knowledge. This project utilizes HTML5, CSS3, and JavaScript to create an interactive learning environment, helping players test their mastery of the game through a responsive and lightweight interface.",
+                image: "/quizchess.jpg",
+                technologies: [
+                    { image: "/icons/html5.svg", alt: "html5" },
+                    { image: "/icons/javascript.svg", alt: "javascript" },
+                    { image: "/icons/css3.svg", alt: "css3" }
+                ]
+            },
+            {
+                title: "Thermal Converter",
+                description: "A robust application for instant conversion between major temperature scales (Celsius, Fahrenheit, and Kelvin). Built with a focus on precise mathematical formulas and reactive state management, ensuring quick and accurate results for scientific or daily use.",
+                image: "/conversortermico.jpg",
+                technologies: [
+                    { image: "/icons/html5.svg", alt: "html5" },
+                    { image: "/icons/javascript.svg", alt: "javascript" },
+                    { image: "/icons/css3.svg", alt: "css3" }
+                ]
+            }
+        ]
     },
     codeCard: {
         name: "name",
